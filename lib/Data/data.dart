09c25e2c5665a6014831befe4d99b820ccb2ec2e
Ryan Romeo <ryanromeo10@gmail.com>
2025-05-29 14:22:37 +0200

    refactoring data save names and download alignment

--- lib/Data/data.dart
+++ lib/Data/data.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:developer' as developer;
 
-import 'package:plantapp/Data/functions.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:plantapp/Data/plant.dart';
 import 'strings.dart';
@@ -30,9 +31,9 @@ class PlantData {
       File f = await File("$loc/$filename").create(recursive: true);
       final p = plants.map((plant) => plant.toJson()).toList();
       f.writeAsString(jsonEncode(p));
-      developer.log("$green[data:writeJson]writing is success$reset");
+      developer.log("$green[data:writeJson] writing is success$reset");
     } catch (_) {
-      developer.log("${red}Exception found$reset");
+      developer.log("$red[data:writeJson] Exception found$reset");
     }
     developer.log("$green[data:wrietJson] directory path is :$loc$reset");
   }
@@ -53,7 +54,9 @@ class PlantData {
         openAppSettings();
       }
       String path = await filelocation;
-      developer.log("$green[data:readJson]creation success$path $status$reset");
+      developer.log(
+        "$green[data:readJson] creation success$path $status$reset",
+      );
       String str = await File("$path/$filename").readAsString();
       // String str = await File("assets\\plantdetails.json").readAsString();
       List<dynamic> plantListJson = await jsonDecode(str);
@@ -71,16 +74,76 @@ class PlantData {
           ),
         );
       }
-      print(plantList.length);
       developer.log(
-        "$green[data:readJson]complete read is success $status$reset",
+        "$green[data:readJson] complete read of ${plantList.length} plants is success $status$reset",
       );
     } on PathNotFoundException catch (_) {
-      developer.log("$red[data:readJson]exception caught while reading$reset");
+      developer.log("$red[data:readJson] exception caught while reading$reset");
     }
     developer.log(
-      "$white[data:readJson]status of the permission is :$status$reset",
+      "$white[data:readJson] status of the permission is :$status$reset",
     );
     return plantList;
   }
+}
+
+Future<String> createFolder(String filename) async {
+  final dir = Directory(
+    '${Platform.isAndroid ?
+        // (await getExternalStorageDirectory())!.path //FOR ANDROID
+        (await getApplicationSupportDirectory()).path : (await getApplicationSupportDirectory()).path //FOR IOS
+        }/$filename',
+  );
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    await Permission.storage.request();
+  }
+  if ((await dir.exists())) {
+    developer.log(
+      "$blue[fabtestfun:createFolder] Already Exists $dir ${await dir.exists()}$reset",
+    );
+    await for (var entity in dir.list(recursive: true, followLinks: false)) {
+      if (entity is File) {
+        developer.log(
+          "$white[fabtestfun:createFolder] File : ${entity.path} ${await dir.exists()}$reset",
+        );
+      } else if (entity is Directory) {
+        developer.log(
+          "$white[fabtestfun:createFolder] Directory : ${entity.path} ${await dir.exists()}$reset",
+        );
+      }
+    }
+    return dir.path;
+  } else {
+    dir.create(recursive: true);
+    developer.log(
+      "$yellow[fabtestfun:createFolder]creation $dir ${await dir.exists()}$reset",
+    );
+    return dir.path;
+  }
+}
+
+Future<dynamic> loadtestdata() async {
+  final String response = await rootBundle.loadString(
+    'assets/plantdetails.json',
+  );
+  final data = await json.decode(response);
+  developer.log("$yellow[fabtestfun:loadtestdata] Test Data : $data $reset");
+  return data["plants"];
+}
+
+saveToDownloads(List<Plant> plants, String filename) async {
+  String loc = generalDownloadDir.path;
+  developer.log(
+    "$green[data:saveToDownloads] before filewriting part$loc$reset",
+  );
+  try {
+    File f = await File("$loc/$filename").create(recursive: true);
+    final p = plants.map((plant) => plant.toJson()).toList();
+    f.writeAsString(jsonEncode(p));
+    developer.log("$green[data:saveToDownloads] writing is success$reset");
+  } catch (_) {
+    developer.log("$red[data:saveToDownloads] Exception found$reset");
+  }
+  developer.log("$green[data:saveToDownloads] directory path is :$loc$reset");
 }
